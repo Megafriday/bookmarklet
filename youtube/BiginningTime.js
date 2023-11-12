d14329c5970f4641355bf36acddf85e1efb86a0b
Megafriday <megafriday@yahoo.co.jp>
2023-11-12 20:07:57 +0900

    ✨ add Youtube bookmarklets

---
+++ youtube/BiginningTime.js
@@ -0,0 +1,2 @@
+// 時間を最初に戻す
+location.href = location.href.replace(/&t=.+s/, "");
---
+++ youtube/ExitChanel.js
@@ -0,0 +1,5 @@
+// チャンネルから抜け出す
+// https://www.youtube.com/watch?v=rAT6DdR5_Cg&list=PLg8AGeLrqYe5aKz20ubqMEYNF525tIMb3&index=1
+
+location.href = location.href.replace(/&list=.+&index=\d+/, "");
+
