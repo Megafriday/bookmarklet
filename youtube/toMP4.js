e5c46657096617840f6c6b2fbbc922198d038c81
Megafriday <megafriday@yahoo.co.jp>
2024-10-26 14:46:37 +0000

    URLの変更

--- youtube/toMP4.js
+++ youtube/toMP4.js
@@ -1,5 +1,5 @@
 // https://www.youtube.com/watch?v=RvRokp3cVsA
-// https://www.y2meta.com/jp/youtube-to-mp3/RvRokp3cVsA
+// https://www.y2meta.com/jp/youtube/RvRokp3cVsA
 const parsedUrl = new URL(window.location.href);
 const id = parsedUrl.searchParams.get("v");
-window.open(`https://www.y2meta.com/jp/youtube-to-mp3/${id}`);
+window.open(`https://www.y2meta.com/jp/youtube/${id}`);
