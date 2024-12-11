c4f7783d53c80d00ec717384e2a2488d9ebdfbf7
Megafriday <megafriday@yahoo.co.jp>
2024-12-11 12:34:35 +0000

    ✨ ChatGPT and Calude の追加

--- AI/perplexity.js
+++ AI/perplexity.js
@@ -1,2 +1,12 @@
-const selected = encodeURIComponent(window.getSelection());
-window.open(`https://www.perplexity.ai/search?s=o&q=${selected}`);
+{
+	const selected = encodeURIComponent(window.getSelection());
+	window.open(`https://www.perplexity.ai/search?s=o&q=${selected}`);
+}
+{
+	const selected = encodeURIComponent(window.getSelection());
+	window.open(`https://chatgpt.com?q=${selected}`);
+}
+{
+	const selected = encodeURIComponent(window.getSelection());
+	window.open(`https://claude.ai/new?q=${selected}`);
+}
